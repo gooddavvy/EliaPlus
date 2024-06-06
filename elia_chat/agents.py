3ac87395ffbf785670b4d78d7da066c0c8e7afb3
David AKIN <110428134+gooddavvy@users.noreply.github.com>
2024-06-06 11:30:16 -0500

    Update agents.py

---
+++ elia_chat/agents.py
@@ -0,0 +1,7 @@
+# WHOOPS!!!
+### There won't be anything here for now.
+## stay tuned
+
+if __name__ == "__main__":
+    from whoops import undefined, agents
+    print(undefined, agents, "There won't be anything here for now.", "stay tuned")
